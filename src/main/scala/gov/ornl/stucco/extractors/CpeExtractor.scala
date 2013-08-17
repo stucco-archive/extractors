194d19789cab21f6534a32de85bfe812e978e997
Anish Athalye <aathalye@me.com>
2013-08-16 21:35:40 -0400

    Update CPE extractor to be more concise
    Use new morph feature (introduced in c3c6927) to make CPE extractor more
    concise.

--- src/main/scala/gov/ornl/stucco/extractors/CpeExtractor.scala
+++ src/main/scala/gov/ornl/stucco/extractors/CpeExtractor.scala
@@ -11,30 +11,31 @@ import morph.extractor.Extractor
 object CpeExtractor extends Extractor {
 
   def extract(node: ValueNode): ValueNode = ^(
-    "vertices" -> (node ~> "cpe-list" ~> "cpe-item" %%-> { item: ValueNode =>
-      val substrings = ((item ~> "@name").asString.split(":"))
+    "vertices" -> (node ~> "cpe-list" ~> "cpe-item" %%-> { item =>
+      val substrings = (item ~> "@name").asString split ":"
       ^(
         "_id" -> item ~> "@name",
         "_type" -> "vertex",
         "vertexType" -> "software",
         "source" -> "CPE",
         //TODO better handling for multiple lang desc., don't just assume english is first.
-        "description" -> {(item ~> "title" ~> "#text") orElse (item ~> "title" ~> 0 ~> "#text")},
+        "description" -> {
+          (item ~> "title" ~> "#text") orElse (item ~> "title" ~> 0 ~> "#text")
+        },
         "nvdId" -> item ~> "meta:item-metadata" ~> "@nvd-id",
         "status" -> item ~> "meta:item-metadata" ~> "@status",
         "modifiedDate" -> item ~> "meta:item-metadata" ~> "@modification-date",
 
         //index 0 is the "source", which is always "cpe", and is redundant with above.
-        "part" -> { if (substrings.length > 1) substrings(1) else None },
-        "vendor" -> { if (substrings.length > 2) substrings(2) else None },
-        "product" -> { if (substrings.length > 3) substrings(3) else None },
-        "version" -> { if (substrings.length > 4) substrings(4) else None },
-        "update" -> { if (substrings.length > 5) substrings(5) else None },
-        "edition" -> { if (substrings.length > 6) substrings(6) else None },
-        "language" -> { if (substrings.length > 7) substrings(7) else None }
-        //TODO: above is not great, but we are likely re-doing how morph does regexes & such soon anyway.
+        "part" -> (substrings lift 1),
+        "vendor" -> (substrings lift 2),
+        "product" -> (substrings lift 3),
+        "version" -> (substrings lift 4),
+        "update" -> (substrings lift 5),
+        "edition" -> (substrings lift 6),
+        "language" -> (substrings lift 7)
+      //TODO: above is not great, but we are likely re-doing how morph does regexes & such soon anyway.
       )
     }).encapsulate
-
   )
 }
