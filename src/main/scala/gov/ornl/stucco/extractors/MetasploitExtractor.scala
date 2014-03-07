988a517fec3833484473a4e40221fa0d9673665b
euf <euf@ornl.gov>
2014-03-07 18:34:06 -0500

    added metasploit extractor & tests

---
+++ src/main/scala/gov/ornl/stucco/extractors/MetasploitExtractor.scala
@@ -0,0 +1,100 @@
+package gov.ornl.stucco.extractors
+
+import morph.ast._
+import morph.extractor.Extractor
+
+object MetasploitExtractor extends Extractor {
+
+  // to make testing easier
+  val O = ObjectNode
+  val A = ArrayNode
+  val S = StringNode
+  val N = NumberNode
+
+  //TODO: it would be useful to also check non-strings here.
+  def notEmpty(node: Option[ValueNode]): Boolean = {
+    node != None && node != Some(S(""))
+  }
+
+  def getRefList(node: Option[ValueNode]): List[String] = { List("asdf","asdf2") }
+
+  def extract(node: ValueNode): ValueNode = {
+    //"id","mtime","file","mtype","refname","fullname","name","rank","description","license","privileged","disclosure_date","default_target","default_action","stance","ready","ref_names","author_names"
+    val headers = node.get(0)
+    val h = headers.asList.zipWithIndex.map { a => a }.toMap
+    ^(
+      "vertices" -> (node mapPartial {
+        //this will ignore header row and will ignore last row if it is just an empty string.
+        case item if (item ~> 0 != headers ~> 0) && (item ~> 1 != None) =>
+          *(
+            {
+              val n = ^(
+                "_id" -> item ~> h("fullname"),
+                "_type" -> "vertex",
+                "source" -> "Metasploit",
+                "vertexType" -> "malware",
+                "malwareType" -> item ~> h("mtype"),
+                "discoveryDate" -> item ~> h("disclosure_date"),
+                "shortDescription" -> item ~> h("name"),
+                "fullDescription" -> item ~> h("description")
+              )
+              if (notEmpty(n ~> "_id")) n
+              else None
+            },
+            {
+              val cves = morph.parser.CsvParser((item ~> h("ref_names")).asString) ~> 0 
+              val m = cves mapPartial {
+                case cve if (cve.isString && cve.asString.contains("CVE-") ) =>
+                *(
+                  {
+                    val n = ^(
+                      "_id" -> cve.asString.trim(),
+                      "_type" -> "vertex",
+                      "source" -> "Metasploit",
+                      "vertexType" -> "vulnerability"
+                    )
+                    if (notEmpty(n ~> "_id") ) n
+                    else None
+                  }
+                )
+              }
+              if (m.asList.length > 0) m.autoFlatten
+              else None
+            }
+          )
+      }).autoFlatten.autoFlatten,
+      "edges" -> (node mapPartial {
+        //this will ignore header row and will ignore last row if it is just an empty string.
+        case item if (item ~> 0 != headers ~> 0) && (item ~> 1 != None) =>
+          *(
+            {
+              val cves = morph.parser.CsvParser((item ~> h("ref_names")).asString) ~> 0 //*((item ~> h("ref_names")).asString.split(","))
+              val m = cves mapPartial {
+                case cve if (cve.isString && cve.asString.contains("CVE-") ) =>
+                *(
+                  {
+                    val n = ^(
+                      "_id" -> Safely { (item ~> h("fullname")).asString + "_exploits_" + cve.asString.trim() },
+                      "_outV" -> item ~> h("fullname"),
+                      "_inV" -> cve.asString.trim(),
+                      "_type" -> "edge",
+                      "_label" -> "exploits",
+                      "source" -> "Metasploit",
+                      "outVType" -> "malware",
+                      "inVType" -> "vulnerability"
+                    )
+                    if (notEmpty(n ~> "_id") ) n
+                    else None
+                  }
+                )
+              }
+              if (m.asList.length > 0) m.autoFlatten//ArrayNode(m.asInstanceOf[List[morph.ast.ValueNode]])//A(m.asInstanceOf[List[ObjectNode]])//(notEmpty(m)) A(m)
+              else None
+            }
+          )
+      }).autoFlatten.autoFlatten        
+    )
+  }
+
+}
+
---
+++ src/test/scala/gov/ornl/stucco/extractors/MetasploitExtractorSuite.scala
@@ -0,0 +1,140 @@
+import org.scalatest.FunSuite
+
+import morph.ast._
+import morph.ast.Implicits._
+import morph.ast.DSL._
+import morph.parser._
+import morph.parser.Interface._
+import morph.utils.Utils._
+
+import gov.ornl.stucco.extractors._
+
+class MetasploitExtractorSuite extends FunSuite {
+
+  // to make testing easier
+  val O = ObjectNode
+  val A = ArrayNode
+  val S = StringNode
+  val N = NumberNode
+
+  test("parse an empty Metasploit element") {
+    var text = """"id","mtime","file","mtype","refname","fullname","name","rank","description","license","privileged","disclosure_date","default_target","default_action","stance","ready","ref_names","author_names"
+-1,,,,,-1,,,,,,,,,,,,
+"""
+    val node = morph.parser.CsvParser(text)
+    val msf = MetasploitExtractor.extract(node)
+
+    assert(msf ~> "vertices" ~> 0 ~> "_id" === Some(S("-1")))
+    assert(msf ~> "vertices" ~> 0 ~> "_type" === Some(S("vertex")))
+    assert(msf ~> "vertices" ~> 0 ~> "source" === Some(S("Metasploit")))
+    assert(msf ~> "vertices" ~> 0 ~> "vertexType" === Some(S("malware")))
+
+    assert(msf ~> "vertices" ~> 1 === None)
+    assert(msf ~> "edges" ~> 0 === None)
+  }
+
+  test("parse 1 Metasploit element - No CVE given") {
+    var text = """"id","mtime","file","mtype","refname","fullname","name","rank","description","license","privileged","disclosure_date","default_target","default_action","stance","ready","ref_names","author_names"
+1,"2013-05-07 00:25:41","/opt/metasploit/apps/pro/msf3/modules/exploits/aix/rpc_cmsd_opcode21.rb","exploit","aix/rpc_cmsd_opcode21","exploit/aix/rpc_cmsd_opcode21","AIX Calendar Manager Service Daemon (rpc.cmsd) Opcode 21 Buffer Overflow",500,"This module exploits a buffer overflow vulnerability in opcode 21 handled by rpc.cmsd on AIX. By making a request with a long string passed to the first argument of the ""rtable_create"" RPC, a stack based buffer overflow occurs. This leads to arbitrary code execution.  NOTE: Unsuccessful attempts may cause inetd/portmapper to enter a state where further attempts are not possible.","Metasploit Framework License (BSD)","f","2009-10-07 00:00:00",0,,"aggressive","t","BID-36615, OSVDB-58726, URL-http://aix.software.ibm.com/aix/efixes/security/cmsd_advisory.asc, URL-http://labs.idefense.com/intelligence/vulnerabilities/display.php?id=825","Rodrigo Rubira Branco (BSDaemon), jduck <jduck@metasploit.com>"
+"""
+    val node = morph.parser.CsvParser(text)
+    val msf = MetasploitExtractor.extract(node)
+
+    assert(msf ~> "vertices" ~> 0 ~> "_id" === Some(S("exploit/aix/rpc_cmsd_opcode21")))
+    assert(msf ~> "vertices" ~> 0 ~> "_type" === Some(S("vertex")))
+    assert(msf ~> "vertices" ~> 0 ~> "source" === Some(S("Metasploit")))
+    assert(msf ~> "vertices" ~> 0 ~> "vertexType" === Some(S("malware")))
+    assert(msf ~> "vertices" ~> 0 ~> "malwareType" === Some(S("exploit")))
+    assert(msf ~> "vertices" ~> 0 ~> "discoveryDate" === Some(S("2009-10-07 00:00:00")))
+    assert(msf ~> "vertices" ~> 0 ~> "shortDescription" === Some(S("AIX Calendar Manager Service Daemon (rpc.cmsd) Opcode 21 Buffer Overflow")))
+    assert(msf ~> "vertices" ~> 0 ~> "fullDescription" === Some(S("This module exploits a buffer overflow vulnerability in opcode 21 handled by rpc.cmsd on AIX. By making a request with a long string passed to the first argument of the \"rtable_create\" RPC, a stack based buffer overflow occurs. This leads to arbitrary code execution.  NOTE: Unsuccessful attempts may cause inetd/portmapper to enter a state where further attempts are not possible.")))
+
+    assert(msf ~> "vertices" ~> 1 === None)
+    assert(msf ~> "edges" ~> 0 === None)
+  }
+
+    test("parse 1 Metasploit element - with CVE") {
+    var text = """"id","mtime","file","mtype","refname","fullname","name","rank","description","license","privileged","disclosure_date","default_target","default_action","stance","ready","ref_names","author_names"
+1,"2013-05-07 00:25:41","/opt/metasploit/apps/pro/msf3/modules/exploits/aix/rpc_cmsd_opcode21.rb","exploit","aix/rpc_cmsd_opcode21","exploit/aix/rpc_cmsd_opcode21","AIX Calendar Manager Service Daemon (rpc.cmsd) Opcode 21 Buffer Overflow",500,"This module exploits a buffer overflow vulnerability in opcode 21 handled by rpc.cmsd on AIX. By making a request with a long string passed to the first argument of the ""rtable_create"" RPC, a stack based buffer overflow occurs. This leads to arbitrary code execution.  NOTE: Unsuccessful attempts may cause inetd/portmapper to enter a state where further attempts are not possible.","Metasploit Framework License (BSD)","f","2009-10-07 00:00:00",0,,"aggressive","t","BID-36615, CVE-2009-3699, OSVDB-58726, URL-http://aix.software.ibm.com/aix/efixes/security/cmsd_advisory.asc, URL-http://labs.idefense.com/intelligence/vulnerabilities/display.php?id=825","Rodrigo Rubira Branco (BSDaemon), jduck <jduck@metasploit.com>"
+"""
+    val node = morph.parser.CsvParser(text)
+    val msf = MetasploitExtractor.extract(node)
+
+    assert(msf ~> "vertices" ~> 0 ~> "_id" === Some(S("exploit/aix/rpc_cmsd_opcode21")))
+    assert(msf ~> "vertices" ~> 0 ~> "_type" === Some(S("vertex")))
+    assert(msf ~> "vertices" ~> 0 ~> "source" === Some(S("Metasploit")))
+    assert(msf ~> "vertices" ~> 0 ~> "vertexType" === Some(S("malware")))
+    assert(msf ~> "vertices" ~> 0 ~> "malwareType" === Some(S("exploit")))
+    assert(msf ~> "vertices" ~> 0 ~> "discoveryDate" === Some(S("2009-10-07 00:00:00")))
+    assert(msf ~> "vertices" ~> 0 ~> "shortDescription" === Some(S("AIX Calendar Manager Service Daemon (rpc.cmsd) Opcode 21 Buffer Overflow")))
+    assert(msf ~> "vertices" ~> 0 ~> "fullDescription" === Some(S("This module exploits a buffer overflow vulnerability in opcode 21 handled by rpc.cmsd on AIX. By making a request with a long string passed to the first argument of the \"rtable_create\" RPC, a stack based buffer overflow occurs. This leads to arbitrary code execution.  NOTE: Unsuccessful attempts may cause inetd/portmapper to enter a state where further attempts are not possible.")))
+
+    assert(msf ~> "vertices" ~> 1 ~> "_id" === Some(S("CVE-2009-3699")))
+    assert(msf ~> "vertices" ~> 1 ~> "_type" === Some(S("vertex")))
+    assert(msf ~> "vertices" ~> 1 ~> "source" === Some(S("Metasploit")))
+    assert(msf ~> "vertices" ~> 1 ~> "vertexType" === Some(S("vulnerability")))
+
+    assert(msf ~> "vertices" ~> 2 === None)
+
+    assert(msf ~> "edges" ~> 0 ~> "_id" === Some(S("exploit/aix/rpc_cmsd_opcode21_exploits_CVE-2009-3699")))
+    assert(msf ~> "edges" ~> 0 ~> "_outV" === Some(S("exploit/aix/rpc_cmsd_opcode21")))
+    assert(msf ~> "edges" ~> 0 ~> "_inV" === Some(S("CVE-2009-3699")))
+    assert(msf ~> "edges" ~> 0 ~> "_type" === Some(S("edge")))
+    assert(msf ~> "edges" ~> 0 ~> "_label" === Some(S("exploits")))
+    assert(msf ~> "edges" ~> 0 ~> "source" === Some(S("Metasploit")))
+    assert(msf ~> "edges" ~> 0 ~> "outVType" === Some(S("malware")))
+    assert(msf ~> "edges" ~> 0 ~> "inVType" === Some(S("vulnerability")))
+
+    assert(msf ~> "edges" ~> 1 === None)
+  }
+
+      test("parse 1 Metasploit element - with two CVEs") {
+    var text = """"id","mtime","file","mtype","refname","fullname","name","rank","description","license","privileged","disclosure_date","default_target","default_action","stance","ready","ref_names","author_names"
+1,"2013-05-07 00:25:41","/opt/metasploit/apps/pro/msf3/modules/exploits/aix/rpc_cmsd_opcode21.rb","exploit","aix/rpc_cmsd_opcode21","exploit/aix/rpc_cmsd_opcode21","AIX Calendar Manager Service Daemon (rpc.cmsd) Opcode 21 Buffer Overflow",500,"This module exploits a buffer overflow vulnerability in opcode 21 handled by rpc.cmsd on AIX. By making a request with a long string passed to the first argument of the ""rtable_create"" RPC, a stack based buffer overflow occurs. This leads to arbitrary code execution.  NOTE: Unsuccessful attempts may cause inetd/portmapper to enter a state where further attempts are not possible.","Metasploit Framework License (BSD)","f","2009-10-07 00:00:00",0,,"aggressive","t","BID-36615, CVE-2009-3699, CVE-2009-nnnn, OSVDB-58726, URL-http://aix.software.ibm.com/aix/efixes/security/cmsd_advisory.asc, URL-http://labs.idefense.com/intelligence/vulnerabilities/display.php?id=825","Rodrigo Rubira Branco (BSDaemon), jduck <jduck@metasploit.com>"
+"""
+    val node = morph.parser.CsvParser(text)
+    val msf = MetasploitExtractor.extract(node)
+
+    assert(msf ~> "vertices" ~> 0 ~> "_id" === Some(S("exploit/aix/rpc_cmsd_opcode21")))
+    assert(msf ~> "vertices" ~> 0 ~> "_type" === Some(S("vertex")))
+    assert(msf ~> "vertices" ~> 0 ~> "source" === Some(S("Metasploit")))
+    assert(msf ~> "vertices" ~> 0 ~> "vertexType" === Some(S("malware")))
+    assert(msf ~> "vertices" ~> 0 ~> "malwareType" === Some(S("exploit")))
+    assert(msf ~> "vertices" ~> 0 ~> "discoveryDate" === Some(S("2009-10-07 00:00:00")))
+    assert(msf ~> "vertices" ~> 0 ~> "shortDescription" === Some(S("AIX Calendar Manager Service Daemon (rpc.cmsd) Opcode 21 Buffer Overflow")))
+    assert(msf ~> "vertices" ~> 0 ~> "fullDescription" === Some(S("This module exploits a buffer overflow vulnerability in opcode 21 handled by rpc.cmsd on AIX. By making a request with a long string passed to the first argument of the \"rtable_create\" RPC, a stack based buffer overflow occurs. This leads to arbitrary code execution.  NOTE: Unsuccessful attempts may cause inetd/portmapper to enter a state where further attempts are not possible.")))
+
+    assert(msf ~> "vertices" ~> 1 ~> "_id" === Some(S("CVE-2009-3699")))
+    assert(msf ~> "vertices" ~> 1 ~> "_type" === Some(S("vertex")))
+    assert(msf ~> "vertices" ~> 1 ~> "source" === Some(S("Metasploit")))
+    assert(msf ~> "vertices" ~> 1 ~> "vertexType" === Some(S("vulnerability")))
+
+    assert(msf ~> "vertices" ~> 2 ~> "_id" === Some(S("CVE-2009-nnnn")))
+    assert(msf ~> "vertices" ~> 2 ~> "_type" === Some(S("vertex")))
+    assert(msf ~> "vertices" ~> 2 ~> "source" === Some(S("Metasploit")))
+    assert(msf ~> "vertices" ~> 2 ~> "vertexType" === Some(S("vulnerability")))
+
+    assert(msf ~> "vertices" ~> 3 === None)
+
+    assert(msf ~> "edges" ~> 0 ~> "_id" === Some(S("exploit/aix/rpc_cmsd_opcode21_exploits_CVE-2009-3699")))
+    assert(msf ~> "edges" ~> 0 ~> "_outV" === Some(S("exploit/aix/rpc_cmsd_opcode21")))
+    assert(msf ~> "edges" ~> 0 ~> "_inV" === Some(S("CVE-2009-3699")))
+    assert(msf ~> "edges" ~> 0 ~> "_type" === Some(S("edge")))
+    assert(msf ~> "edges" ~> 0 ~> "_label" === Some(S("exploits")))
+    assert(msf ~> "edges" ~> 0 ~> "source" === Some(S("Metasploit")))
+    assert(msf ~> "edges" ~> 0 ~> "outVType" === Some(S("malware")))
+    assert(msf ~> "edges" ~> 0 ~> "inVType" === Some(S("vulnerability")))
+
+    assert(msf ~> "edges" ~> 1 ~> "_id" === Some(S("exploit/aix/rpc_cmsd_opcode21_exploits_CVE-2009-nnnn")))
+    assert(msf ~> "edges" ~> 1 ~> "_outV" === Some(S("exploit/aix/rpc_cmsd_opcode21")))
+    assert(msf ~> "edges" ~> 1 ~> "_inV" === Some(S("CVE-2009-nnnn")))
+    assert(msf ~> "edges" ~> 1 ~> "_type" === Some(S("edge")))
+    assert(msf ~> "edges" ~> 1 ~> "_label" === Some(S("exploits")))
+    assert(msf ~> "edges" ~> 1 ~> "source" === Some(S("Metasploit")))
+    assert(msf ~> "edges" ~> 1 ~> "outVType" === Some(S("malware")))
+    assert(msf ~> "edges" ~> 1 ~> "inVType" === Some(S("vulnerability")))
+
+    assert(msf ~> "edges" ~> 2 === None)
+  }
+}
+
