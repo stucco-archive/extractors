e926e1f2316245f6e096d9caa262529c7d925906
euf <euf@ornl.gov>
2015-07-15 18:05:16 -0400

    add additional situ test case

--- src/test/scala/gov/ornl/stucco/extractors/SituCyboxExtractorSuite.scala
+++ src/test/scala/gov/ornl/stucco/extractors/SituCyboxExtractorSuite.scala
@@ -17,7 +17,7 @@ class SituCyboxExtractorSuite extends FunSuite {
   val S = StringNode
   val N = NumberNode
 
-  test("parse one record") {
+  test("parse one record - no bytes or counts") {
     val node = XmlParser("""
         <cybox:Observables xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:cybox="http://cybox.mitre.org/cybox-2" xmlns:cyboxCommon="http://cybox.mitre.org/common-2" xmlns:NetFlowObj="http://cybox.mitre.org/objects#NetworkFlowObject-2" xmlns:SocketAddressObj="http://cybox.mitre.org/objects#SocketAddressObject-1" xmlns:AddressObj="http://cybox.mitre.org/objects#AddressObject-2" xmlns:PortObj="http://cybox.mitre.org/objects#PortObject-2">
             <cybox:Observable id="6e104843-1c1f-5c76-87c0-4647915aa251">
@@ -109,7 +109,7 @@ class SituCyboxExtractorSuite extends FunSuite {
     assert(entries ~> "vertices" ~> 6 ~> "vertexType" === Some(S("port")))
     assert(entries ~> "vertices" ~> 6 ~> "source" === Some(S("situ")))
 
-    assert(entries ~> "edges" ~> 7 === None)
+    assert(entries ~> "vertices" ~> 7 === None)
 
     assert(entries ~> "edges" ~> 0 ~> "_id" === Some(S("128.219.49.14:48980::52.5.45.166:80_srcAddress_128.219.49.14:48980")))
     assert(entries ~> "edges" ~> 0 ~> "description" === Some(S("128.219.49.14, port 48980 to 52.5.45.166, port 80 has source address 128.219.49.14, port 48980")))
@@ -165,6 +165,187 @@ class SituCyboxExtractorSuite extends FunSuite {
     assert(entries ~> "edges" ~> 5 ~> "description" === Some(S("52.5.45.166, port 80 has port 80")))
     assert(entries ~> "edges" ~> 5 ~> "_outV" === Some(S("52.5.45.166:80")))
     assert(entries ~> "edges" ~> 5 ~> "_inV" === Some(S("80")))
+    assert(entries ~> "edges" ~> 5 ~> "_type" === Some(S("edge")))
+    assert(entries ~> "edges" ~> 5 ~> "_label" === Some(S("hasPort")))
+    assert(entries ~> "edges" ~> 5 ~> "source" === Some(S("situ")))
+    assert(entries ~> "edges" ~> 5 ~> "outVType" === Some(S("address")))
+    assert(entries ~> "edges" ~> 5 ~> "inVType" === Some(S("port")))
+
+    assert(entries ~> "edges" ~> 6 === None)
+
+  }
+
+  test("parse one complete record") {
+    val node = XmlParser("""
+        <cybox:Observables xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:cybox="http://cybox.mitre.org/cybox-2" xmlns:cyboxCommon="http://cybox.mitre.org/common-2" xmlns:NetFlowObj="http://cybox.mitre.org/objects#NetworkFlowObject-2" xmlns:SocketAddressObj="http://cybox.mitre.org/objects#SocketAddressObject-1" xmlns:AddressObj="http://cybox.mitre.org/objects#AddressObject-2" xmlns:PortObj="http://cybox.mitre.org/objects#PortObject-2">
+            <cybox:Observable id="de305d54-75b4-431b-adb2-eb6b9e546013">
+                <cybox:Description>SITU Network Flow Observable</cybox:Description>
+                <cybox:Object id="de305d54-75b4-431b-adb2-eb6b9e546013">
+                    <cybox:Properties xsi:type="NetFlowObj:NetworkFlowObjectType">
+                        <NetFlowObj:Network_Flow_Label>
+                            <NetFlowObj:Source_Socket_Address>
+                                <SocketAddressObj:IP_Address>
+                                    <AddressObj:Address_Value>1.2.3.4</AddressObj:Address_Value>
+                                </SocketAddressObj:IP_Address>
+                                <SocketAddressObj:Port>
+                                    <PortObj:Port_Value>1000</PortObj:Port_Value>
+                                </SocketAddressObj:Port>
+                            </NetFlowObj:Source_Socket_Address>
+                            <NetFlowObj:Destination_Socket_Address>
+                                <SocketAddressObj:IP_Address>
+                                    <AddressObj:Address_Value>5.6.7.8</AddressObj:Address_Value>
+                                </SocketAddressObj:IP_Address>
+                                <SocketAddressObj:Port>
+                                    <PortObj:Port_Value>1001</PortObj:Port_Value>
+                                </SocketAddressObj:Port>
+                            </NetFlowObj:Destination_Socket_Address>
+                            <NetFlowObj:IP_Protocol>proto</NetFlowObj:IP_Protocol>
+                        </NetFlowObj:Network_Flow_Label>
+                        <NetFlowObj:Cooperative_Protection_Program_Record>
+                            <Cooperative_Protection_Program:Duration>100</Cooperative_Protection_Program:Duration>
+                            <Cooperative_Protection_Program:SrcAppBytes>101</Cooperative_Protection_Program:SrcAppBytes>
+                            <Cooperative_Protection_Program:DstAppBytes>102</Cooperative_Protection_Program:DstAppBytes>
+                            <Cooperative_Protection_Program:AppBytes>103</Cooperative_Protection_Program:AppBytes>
+                            <Cooperative_Protection_Program:SrcBytes>104</Cooperative_Protection_Program:SrcBytes>
+                            <Cooperative_Protection_Program:DstBytes>105</Cooperative_Protection_Program:DstBytes>
+                            <Cooperative_Protection_Program:Bytes>106</Cooperative_Protection_Program:Bytes>
+                            <Cooperative_Protection_Program:SrcPackets>107</Cooperative_Protection_Program:SrcPackets>
+                            <Cooperative_Protection_Program:DstPackets>108</Cooperative_Protection_Program:DstPackets>
+                            <Cooperative_Protection_Program:Packets>109</Cooperative_Protection_Program:Packets>
+                        </NetFlowObj:Cooperative_Protection_Program_Record>
+                        <NetFlowObj:SITU_Score>9.876543</NetFlowObj:SITU_Score>
+                    </cybox:Properties>
+                </cybox:Object>
+            </cybox:Observable>
+        </cybox:Observables>
+      """)
+    //println(node)
+    val entries = SituCyboxExtractor(node)
+    //println(entries)
+
+    assert(entries ~> "vertices" ~> 0 ~> "_id" === Some(S("1.2.3.4:1000::5.6.7.8:1001")))
+    assert(entries ~> "vertices" ~> 0 ~> "name" === Some(S("1.2.3.4:1000::5.6.7.8:1001")))
+    assert(entries ~> "vertices" ~> 0 ~> "description" === Some(S("1.2.3.4, port 1000 to 5.6.7.8, port 1001")))
+    assert(entries ~> "vertices" ~> 0 ~> "_type" === Some(S("vertex")))
+    assert(entries ~> "vertices" ~> 0 ~> "vertexType" === Some(S("flow")))
+    assert(entries ~> "vertices" ~> 0 ~> "source" === Some(S("situ")))
+    assert(entries ~> "vertices" ~> 0 ~> "proto" === Some(S("proto")))
+    assert(entries ~> "vertices" ~> 0 ~> "situScore" === Some(N(9.876543)))
+
+    //assert(entries ~> "vertices" ~> 0 ~> "duration" === Some(S("100")))
+    //assert(entries ~> "vertices" ~> 0 ~> "srcAppBytes" === Some(S("101")))
+    //assert(entries ~> "vertices" ~> 0 ~> "dstAppBytes" === Some(S("102")))
+    //assert(entries ~> "vertices" ~> 0 ~> "appBytes" === Some(S("103")))
+    //assert(entries ~> "vertices" ~> 0 ~> "srcBytes" === Some(S("104")))
+    //assert(entries ~> "vertices" ~> 0 ~> "dstBytes" === Some(S("105")))
+    //assert(entries ~> "vertices" ~> 0 ~> "bytes" === Some(S("106")))
+    //assert(entries ~> "vertices" ~> 0 ~> "srcPackets" === Some(S("107")))
+    //assert(entries ~> "vertices" ~> 0 ~> "dstPackets" === Some(S("108")))
+    //assert(entries ~> "vertices" ~> 0 ~> "packets" === Some(S("109")))
+    
+    //assert(entries ~> "vertices" ~> 0 ~> "startTime" === Some(N(1373553586136L)))
+    //assert(entries ~> "vertices" ~> 0 ~> "dir" === Some(S("   ->")))
+    //assert(entries ~> "vertices" ~> 0 ~> "flags" === Some(S(" e s      ")))
+    //assert(entries ~> "vertices" ~> 0 ~> "state" === Some(S("REQ")))
+
+    assert(entries ~> "vertices" ~> 1 ~> "_id" === Some(S("1.2.3.4:1000")))
+    assert(entries ~> "vertices" ~> 1 ~> "name" === Some(S("1.2.3.4:1000")))
+    assert(entries ~> "vertices" ~> 1 ~> "description" === Some(S("1.2.3.4, port 1000")))
+    assert(entries ~> "vertices" ~> 1 ~> "_type" === Some(S("vertex")))
+    assert(entries ~> "vertices" ~> 1 ~> "vertexType" === Some(S("address")))
+    assert(entries ~> "vertices" ~> 1 ~> "source" === Some(S("situ")))
+
+    assert(entries ~> "vertices" ~> 2 ~> "_id" === Some(S("5.6.7.8:1001")))
+    assert(entries ~> "vertices" ~> 2 ~> "name" === Some(S("5.6.7.8:1001")))
+    assert(entries ~> "vertices" ~> 2 ~> "description" === Some(S("5.6.7.8, port 1001")))
+    assert(entries ~> "vertices" ~> 2 ~> "_type" === Some(S("vertex")))
+    assert(entries ~> "vertices" ~> 2 ~> "vertexType" === Some(S("address")))
+    assert(entries ~> "vertices" ~> 2 ~> "source" === Some(S("situ")))
+
+    assert(entries ~> "vertices" ~> 3 ~> "_id" === Some(S("1.2.3.4")))
+    assert(entries ~> "vertices" ~> 3 ~> "name" === Some(S("1.2.3.4")))
+    assert(entries ~> "vertices" ~> 3 ~> "description" === Some(S("1.2.3.4")))
+    assert(entries ~> "vertices" ~> 3 ~> "_type" === Some(S("vertex")))
+    assert(entries ~> "vertices" ~> 3 ~> "vertexType" === Some(S("IP")))
+    assert(entries ~> "vertices" ~> 3 ~> "source" === Some(S("situ")))
+
+    assert(entries ~> "vertices" ~> 4 ~> "_id" === Some(S("5.6.7.8")))
+    assert(entries ~> "vertices" ~> 4 ~> "name" === Some(S("5.6.7.8")))
+    assert(entries ~> "vertices" ~> 4 ~> "description" === Some(S("5.6.7.8")))
+    assert(entries ~> "vertices" ~> 4 ~> "_type" === Some(S("vertex")))
+    assert(entries ~> "vertices" ~> 4 ~> "vertexType" === Some(S("IP")))
+    assert(entries ~> "vertices" ~> 4 ~> "source" === Some(S("situ")))
+
+    assert(entries ~> "vertices" ~> 5 ~> "_id" === Some(S("1000")))
+    assert(entries ~> "vertices" ~> 5 ~> "name" === Some(S("1000")))
+    assert(entries ~> "vertices" ~> 5 ~> "description" === Some(S("1000")))
+    assert(entries ~> "vertices" ~> 5 ~> "_type" === Some(S("vertex")))
+    assert(entries ~> "vertices" ~> 5 ~> "vertexType" === Some(S("port")))
+    assert(entries ~> "vertices" ~> 5 ~> "source" === Some(S("situ")))
+
+    assert(entries ~> "vertices" ~> 6 ~> "_id" === Some(S("1001")))
+    assert(entries ~> "vertices" ~> 6 ~> "name" === Some(S("1001")))
+    assert(entries ~> "vertices" ~> 6 ~> "description" === Some(S("1001")))
+    assert(entries ~> "vertices" ~> 6 ~> "_type" === Some(S("vertex")))
+    assert(entries ~> "vertices" ~> 6 ~> "vertexType" === Some(S("port")))
+    assert(entries ~> "vertices" ~> 6 ~> "source" === Some(S("situ")))
+
+    assert(entries ~> "vertices" ~> 7 === None)
+
+    assert(entries ~> "edges" ~> 0 ~> "_id" === Some(S("1.2.3.4:1000::5.6.7.8:1001_srcAddress_1.2.3.4:1000")))
+    assert(entries ~> "edges" ~> 0 ~> "description" === Some(S("1.2.3.4, port 1000 to 5.6.7.8, port 1001 has source address 1.2.3.4, port 1000")))
+    assert(entries ~> "edges" ~> 0 ~> "_outV" === Some(S("1.2.3.4:1000::5.6.7.8:1001")))
+    assert(entries ~> "edges" ~> 0 ~> "_inV" === Some(S("1.2.3.4:1000")))
+    assert(entries ~> "edges" ~> 0 ~> "_type" === Some(S("edge")))
+    assert(entries ~> "edges" ~> 0 ~> "_label" === Some(S("srcAddress")))
+    assert(entries ~> "edges" ~> 0 ~> "source" === Some(S("situ")))
+    assert(entries ~> "edges" ~> 0 ~> "outVType" === Some(S("flow")))
+    assert(entries ~> "edges" ~> 0 ~> "inVType" === Some(S("address")))
+    
+    assert(entries ~> "edges" ~> 1 ~> "_id" === Some(S("1.2.3.4:1000::5.6.7.8:1001_dstAddress_5.6.7.8:1001")))
+    assert(entries ~> "edges" ~> 1 ~> "description" === Some(S("1.2.3.4, port 1000 to 5.6.7.8, port 1001 has destination address 5.6.7.8, port 1001")))
+    assert(entries ~> "edges" ~> 1 ~> "_outV" === Some(S("1.2.3.4:1000::5.6.7.8:1001")))
+    assert(entries ~> "edges" ~> 1 ~> "_inV" === Some(S("5.6.7.8:1001")))
+    assert(entries ~> "edges" ~> 1 ~> "_type" === Some(S("edge")))
+    assert(entries ~> "edges" ~> 1 ~> "_label" === Some(S("dstAddress")))
+    assert(entries ~> "edges" ~> 1 ~> "source" === Some(S("situ")))
+    assert(entries ~> "edges" ~> 1 ~> "outVType" === Some(S("flow")))
+    assert(entries ~> "edges" ~> 1 ~> "inVType" === Some(S("address")))
+
+    assert(entries ~> "edges" ~> 2 ~> "_id" === Some(S("1.2.3.4:1000_hasIP_1.2.3.4")))
+    assert(entries ~> "edges" ~> 2 ~> "description" === Some(S("1.2.3.4, port 1000 has IP 1.2.3.4")))
+    assert(entries ~> "edges" ~> 2 ~> "_outV" === Some(S("1.2.3.4:1000")))
+    assert(entries ~> "edges" ~> 2 ~> "_inV" === Some(S("1.2.3.4")))
+    assert(entries ~> "edges" ~> 2 ~> "_type" === Some(S("edge")))
+    assert(entries ~> "edges" ~> 2 ~> "_label" === Some(S("hasIP")))
+    assert(entries ~> "edges" ~> 2 ~> "source" === Some(S("situ")))
+    assert(entries ~> "edges" ~> 2 ~> "outVType" === Some(S("address")))
+    assert(entries ~> "edges" ~> 2 ~> "inVType" === Some(S("IP")))
+
+    assert(entries ~> "edges" ~> 3 ~> "_id" === Some(S("5.6.7.8:1001_hasIP_5.6.7.8")))
+    assert(entries ~> "edges" ~> 3 ~> "description" === Some(S("5.6.7.8, port 1001 has IP 5.6.7.8")))
+    assert(entries ~> "edges" ~> 3 ~> "_outV" === Some(S("5.6.7.8:1001")))
+    assert(entries ~> "edges" ~> 3 ~> "_inV" === Some(S("5.6.7.8")))
+    assert(entries ~> "edges" ~> 3 ~> "_type" === Some(S("edge")))
+    assert(entries ~> "edges" ~> 3 ~> "_label" === Some(S("hasIP")))
+    assert(entries ~> "edges" ~> 3 ~> "source" === Some(S("situ")))
+    assert(entries ~> "edges" ~> 3 ~> "outVType" === Some(S("address")))
+    assert(entries ~> "edges" ~> 3 ~> "inVType" === Some(S("IP")))
+    
+    assert(entries ~> "edges" ~> 4 ~> "_id" === Some(S("1.2.3.4:1000_hasPort_1000")))
+    assert(entries ~> "edges" ~> 4 ~> "description" === Some(S("1.2.3.4, port 1000 has port 1000")))
+    assert(entries ~> "edges" ~> 4 ~> "_outV" === Some(S("1.2.3.4:1000")))
+    assert(entries ~> "edges" ~> 4 ~> "_inV" === Some(S("1000")))
+    assert(entries ~> "edges" ~> 4 ~> "_type" === Some(S("edge")))
+    assert(entries ~> "edges" ~> 4 ~> "_label" === Some(S("hasPort")))
+    assert(entries ~> "edges" ~> 4 ~> "source" === Some(S("situ")))
+    assert(entries ~> "edges" ~> 4 ~> "outVType" === Some(S("address")))
+    assert(entries ~> "edges" ~> 4 ~> "inVType" === Some(S("port")))
+
+    assert(entries ~> "edges" ~> 5 ~> "_id" === Some(S("5.6.7.8:1001_hasPort_1001")))
+    assert(entries ~> "edges" ~> 5 ~> "description" === Some(S("5.6.7.8, port 1001 has port 1001")))
+    assert(entries ~> "edges" ~> 5 ~> "_outV" === Some(S("5.6.7.8:1001")))
+    assert(entries ~> "edges" ~> 5 ~> "_inV" === Some(S("1001")))
     assert(entries ~> "edges" ~> 5 ~> "_type" === Some(S("edge")))
     assert(entries ~> "edges" ~> 5 ~> "_label" === Some(S("hasPort")))
     assert(entries ~> "edges" ~> 5 ~> "source" === Some(S("situ")))
